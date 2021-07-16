97dd0a95b3587051b5a4df8ab9ecfecdd4c711d1
thinkerRing <thinkerring2020@gmail.com>
2021-07-16 10:24:16 +0900

    Updates

---
+++ precache-manifest.1d61d6254c69161facce46121f5ff223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a651d1639b11a6054555fef81f8811",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "08c6c7fe888131c5021a",
+    "url": "/habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c6c7fe888131c5021a",
+    "url": "/habit_tracker/static/js/main.bd9fe4e3.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
